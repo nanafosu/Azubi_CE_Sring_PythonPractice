d444cf62d160696756092f7fbe95c0f993bb982d
Kwadwo Fosu Appiah <62400426+nanafosu@users.noreply.github.com>
2023-05-18 07:15:46 +0000

    Practicing ascending array arrangement

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+country = input("What is your country?: ")
+print("Welcome " + name + ", I see you are from " + country + " and you are " + age + "years old.")
