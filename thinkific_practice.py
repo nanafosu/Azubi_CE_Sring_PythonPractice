2720a2513a31ae85cdf2770d93dd7bcc5f4601f9
Kwadwo Fosu Appiah <62400426+nanafosu@users.noreply.github.com>
2023-05-21 08:52:40 +0000

    second commit

---
+++ thinkific_practice.py
@@ -0,0 +1,11 @@
+first_name = "Kwadwo"
+last_name = "Appiah"
+print(first_name + ' ' + last_name)
+output_1 = 'Hello, ' + first_name + ' ' + last_name
+output_2 = 'Hello, {} {}'.format(first_name, last_name)
+output_3 = 'Hello, {0} {1}'.format(first_name, last_name)
+output_4 = f'Hello, {first_name} {last_name}'
+print(output_1)
+print(output_2)
+print(output_3)
+print(output_4)
