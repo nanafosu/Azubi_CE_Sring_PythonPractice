443787d4f4e623ea16e7534f01c47d90d886b63d
Kwadwo Fosu Appiah <62400426+nanafosu@users.noreply.github.com>
2023-05-29 08:55:19 +0000

    Codes i have practiced

---
+++ list_array_Dict.py
@@ -0,0 +1,16 @@
+christoper = {}
+christoper['first'] = 'Christopher'
+christoper['last'] = 'Harrison'
+
+susan = {}
+susan['first'] = 'Susan'
+susan['last'] = 'Ibach'
+
+people = []
+people.append(christoper)
+people.append(susan)
+people.append({ 
+    'first' : 'Bill', 'last' : 'Gates'
+})
+
+print(people)
